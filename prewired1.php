7f290e1be038bbd8ccc0f45b17bd3b6b0bb19500
Amy Guy <amy@rhiaro.co.uk>
2014-03-12 14:55:47 +0000

    Init

---
+++ prewired1.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Rosario:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body class="light-bg">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        <main>
+            <div class="wrapper clearfix">
+
+                <header class="w1of1 clearfix align-center">
+                    <a href="http://prewired.org"><img src="img/scotlandcol2.png" alt="" role="presentation" class="logo" /></a>
+                    <h1>Prewired 1</h1>
+                    <h2>9:30-12:30, 16 October, Informatics Forum</h2>
+                </header>
+                <div class="w1of1 clearfix"><div class="inner">
+
+                    <p><strong>Thanks to the 38 young people who came and the 15 mentors who gave talks and/or support!</strong></p>
+
+                    <h3>Resources</h3>
+                    <p>Here are some links you might want from today.  If there's anything missing, email hello@prewired.org to let me (Amy) know!</p>
+                    <p>Beginning HTML and CSS:</p>
+                    <ul class="subp">
+                        <li><p><a href="http://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro">A handy tool for demo-ing your code directly in the browser</a></p></li>
+                        <li><p><a href="https://github.com/ewan-klein/prewired/tree/master/html">Code and cheatsheet from slides</a></p></li>
+                    </ul>
+                    <p>HTML5 Geolocation:</p>
+                    <ul class="subp">
+                        <li><p><a href="https://github.com/ewan-klein/prewired/tree/master/geolocation">Code examples</a></p></li>
+                        <li><p><a href="http://diveintohtml5.info/geolocation.html">All you could need to know</a></p></li>
+                        <li><p><a href="https://developers.google.com/maps/documentation/javascript/">The Google Maps API</a></p></li>
+                    </ul>
+                    <p>Building a chatbot with Python's Natural Language Toolkit:</p>
+                    <ul class="subp">
+                        <li><p><a href="http://nltk.org/install.html">Installing NLTK</a></p></li>
+                        <li><p><a href="https://github.com/ewan-klein/prewired/tree/master/chatbot">How to</a></p></li>
+                    </ul>
+                    <p>Intro to Scratch:</p>
+                    <ul class="subp">
+                        <li><p><a href="http://scratch.mit.edu/">Make Scratch programs here</a></p></li>
+                        <li><p><a href="http://info.scratch.mit.edu/Video_Tutorials">Video tutorials</a></p></li>
+                    </ul>
+                    <p>Minecraft Redstone Circuits:</p>
+                    <ul class="subp">
+                        <li><p><a href="http://minecraft.gamepedia.com/Tutorials/Advanced_redstone_circuits">Some tutorials</a></p></li>
+                    </ul>
+                    <p>Intro to PyGame:</p>
+                    <ul class="subp">
+                        <li><p>Coming soon...</p></li>
+                    </ul>
+
+                    <h3>Photos</h3>
+                    <p>You can join the <a href="http://www.flickr.com/groups/2346001@N20/">Flickr group</a> if you have some of your own photos you'd like to add.</p>
+                    <object width="400" height="300"> <param name="flashvars" value="offsite=true&lang=en-us&page_show_url=%2Fgroups%2F2346001%40N20%2Fpool%2Fshow%2F&page_show_back_url=%2Fgroups%2F2346001%40N20%2Fpool%2F&group_id=2346001@N20&jump_to=&start_index="></param> <param name="movie" value="http://www.flickr.com/apps/slideshow/show.swf?v=124984"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="http://www.flickr.com/apps/slideshow/show.swf?v=124984" allowFullScreen="true" flashvars="offsite=true&lang=en-us&page_show_url=%2Fgroups%2F2346001%40N20%2Fpool%2Fshow%2F&page_show_back_url=%2Fgroups%2F2346001%40N20%2Fpool%2F&group_id=2346001@N20&jump_to=&start_index=" width="400" height="300"></embed></object>
+                    <p>If you've written a blog post about the event, then let me know and I'll post a link to it here.</p>
+                    
+                    <h3>Schedule</h3>
+                    <p>09:30-10:00 &nbsp; Arrival and registrations</p>
+                    <p>10:00-10:15 &nbsp;&nbsp; Welcome and introductions</p>
+                    <p>10:15-10:35 &nbsp;&nbsp; 5 minute talks (subject to change):</p>
+                    <ul class="subp">
+                        <li><p>Your first website: Introduction to HTML and CSS</p></li>
+                        <li><p>Geolocation with HTML5</p></li>
+                        <li><p>Building a chatbot in NLTK (Natural Language Toolkit, Python)*</p></li>
+                    </ul>
+                    <p>10:35-11:15 &nbsp;&nbsp; Hands on sessions (choose one of the three topics to find out more about)</p>
+                    <p>11:15-11:30 &nbsp;&nbsp; Break for snacks, juice and to see the Quadcopter and Robotic Arm</p>
+                    <p>11:30-11:50 &nbsp;&nbsp; 5 minute talks (subject to change):</p>
+                    <ul class="subp">
+                        <li><p>Introduction to Scratch</p></li>
+                        <li><p>Introduction to PyGame*</p></li>
+                        <li><p>Digital circuitry with Red Stone in Minecraft**</p></li>
+                    </ul>
+                    <p>11:50-12:30 &nbsp;&nbsp; Hands on sessions (choose one of the three topics to find out more about)</p>
+                    <p>12:30 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; End</p>
+
+                    <p class="wee unpad">* Check you have Python 2.7 installed, and install it if you don't (<a href="https://wiki.python.org/moin/BeginnersGuide/Download">instructions</a>).</p>
+                    <p class="wee unpad">** It would help to have a Minecraft license and have it installed for this one.</p>
+
+
+                </div></div>
+                <footer class="w1of1 dark-top-border clearfix"><div class="inner">
+                    <p><strong>Prewired</strong> is being initiated by <a href="http://twitter.com/ewanhklein">Ewan Klein</a>, <a href="http://twitter.com/FredaOByrne">Freda O'Byrne</a>, <a href="http://twitter.com/rhiaro">Amy Guy</a>, <a href="http://twitter.com/kitbarnes">Kit Barnes</a> and <a href="http://www.inf.ed.ac.uk/people/staff/Stuart_Anderson.html">Stuart Anderson</a> and is supported by the University of Edinburgh School of Informatics, and Google.</p>
+                </div></footer>
+            </div>
+        </main>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-44103814-1', 'prewired.org');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
---
+++ prewired2.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Rosario:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body class="light-bg">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        <main>
+            <div class="wrapper clearfix">
+
+                <header class="w1of1 clearfix align-center">
+                    <a href="http://prewired.org"><img src="img/scotlandcol2.png" alt="" role="presentation" class="logo" /></a>
+                    <h1>Prewired 2</h1>
+                    <h2>17:30-19:30, 30 October, Informatics Forum</h2>
+                </header>
+                <div class="w1of1 clearfix"><div class="inner">
+
+                    <p><strong>Thanks to the 21 young people who came and the 10 mentors who gave talks and support!</strong></p>
+
+                    <h3>Resources</h3>
+                    <p>Here are some links you might want from today.  If there's anything missing, email hello@prewired.org to let me (Amy) know!</p>
+                    <p>Web front-end best practices:</p>
+                    <ul class="subp">
+                        <li><p><a href="https://github.com/ewan-klein/prewired/tree/master/webfrontend">Code, links and tips from on screen</a></p></li>
+                    </ul>
+                    <p>Python:</p>
+                    <ul class="subp">
+                        <li><p>Coming soon...</p></li>
+                    </ul>
+                    <p>Java:</p>
+                    <ul class="subp">
+                        <li><p>Coming soon...</p></li>
+                    </ul>
+
+                    <h3>Photos</h3>
+                    <p>We didn't manage to take any this week... If you took your own, then send them to us!</p>
+                    <p>If you've written a blog post about the event, then let me know and I'll post a link to it here.</p>
+
+                </div></div>
+                <footer class="w1of1 dark-top-border clearfix"><div class="inner">
+                    <p><strong>Prewired</strong> is being initiated by <a href="http://twitter.com/ewanhklein">Ewan Klein</a>, <a href="http://twitter.com/FredaOByrne">Freda O'Byrne</a>, <a href="http://twitter.com/rhiaro">Amy Guy</a>, <a href="http://twitter.com/kitbarnes">Kit Barnes</a> and <a href="http://www.inf.ed.ac.uk/people/staff/Stuart_Anderson.html">Stuart Anderson</a> and is supported by the University of Edinburgh School of Informatics, and Google.</p>
+                </div></footer>
+            </div>
+        </main>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-44103814-1', 'prewired.org');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
