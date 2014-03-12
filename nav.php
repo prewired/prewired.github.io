a42c0b2e69847a1be8ab261c49469ad2d42b3fc2
Amy Guy <amy@rhiaro.co.uk>
2014-03-12 17:02:58 +0000

    Tweaks

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav role="navigation" class="dark-bg light clearfix" id="nav">
+	<ul class="top-menu wrapper">
+		<li><a href="#top">Home</a></li>
+		<li><a href="#next">Future dates</a></li>
+		<li><a href="#prev">Past Prewired</a></li>
+		<li><a href="#about">What, why, who and where?</a></li>
+		<li><a href="#contact">Contact</a></li>
+	</ul>
+</nav>
