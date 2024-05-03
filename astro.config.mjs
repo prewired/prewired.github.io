4c3be13a374499b3afc5f8e6b29739cd04967e9b
Rayo Verweij <rayo.verweij@live.nl>
2024-05-03 12:10:59 +0100

    Initialisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx(), sitemap()]
+});
